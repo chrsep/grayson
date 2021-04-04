0f32b52a3c1d672897e2061045035692310b7f71
Chrisando Pramudhita <chrisep8@gmail.com>
2021-04-04 15:17:49 +0700

    update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,11 @@
-<script lang="ts">
-  import Counter from "$lib/Counter.svelte"
-</script>
+<nav class="pb-3 border-b">
+  <div class="flex p-3">
+    <div>Menu</div>
+    <h1 class="flex-grow text-center">Why World!</h1>
+    <div>Profile</div>
+  </div>
 
-<main>
-  <h1>Why world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to
-    build Svelte apps.
-  </p>
-</main>
-
-<style style lang="postcss">
-  main {
-    @apply text-center;
-    @apply p-4;
-    @apply mx-auto;
-  }
-
-  h1 {
-    @apply text-red-600;
-    @apply uppercase;
-    @apply text-6xl;
-    @apply font-thin;
-    @apply leading-tight;
-    @apply my-16 mx-auto;
-    @apply max-w-xs;
-  }
-
-  p {
-    @apply max-w-xs;
-    @apply my-8 mx-auto;
-    @apply leading-snug;
-  }
-
-  @screen sm {
-    h1 {
-      @apply max-w-none;
-    }
-
-    p {
-      @apply max-w-none;
-    }
-  }
-</style>
+  <div class="px-5 py-3 m-3 border rounded-full bg-gray-100">
+    Search
+  </div>
+</nav>
