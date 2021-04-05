26a70683889ea0a9c66730749dce3faac321cad3
Chrisando Pramudhita <chrsep@protonmail.com>
2021-04-05 19:11:47 +0700

    load session on index.js.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,45 @@
 <script context="module" lang="ts">
-  export const prerender = true
+  import type { Load } from "@sveltejs/kit"
+
+  export const load: Load = async ({ session }) => {
+    if (session.user !== null) {
+      return {
+        status: 200,
+        props: {
+          user: session.user
+        }
+      }
+    }
+  }
 </script>
 
-<script>
+<script lang="ts">
   import Button from "$lib/Button.svelte"
-  import { MenuIcon } from "svelte-feather-icons"
+  import { UserIcon, MenuIcon } from "svelte-feather-icons/src"
+  import type { User } from "$lib/domain"
+
+  export let user: User
 </script>
 
 <nav class="pb-3 border-b">
   <div class="flex p-3 items-center">
     <Button>
-      <MenuIcon class="w-6" />
+      <MenuIcon class="w-4" />
     </Button>
 
     <h1 class="flex-grow text-center">Why World!</h1>
 
-    <a href="/login">
-      <Button>Masuk</Button>
-    </a>
+    {#if user}
+      <a href="/account">
+        <Button>
+          <UserIcon class="w-4" />
+        </Button>
+      </a>
+    {:else}
+      <a href="/login">
+        <Button>Masuk</Button>
+      </a>
+    {/if}
   </div>
 
   <div class="px-5 py-3 m-3 border rounded-full bg-gray-100">Cari produk</div>
