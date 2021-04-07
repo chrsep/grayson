ab72eb6f2cb5aa9927f4fac6c700a5ca221b4875
Chrisando Pramudhita <chrsep@protonmail.com>
2021-04-08 02:11:13 +0700

    add back button.

---
+++ src/routes/settings/store/new.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { ChevronLeftIcon } from "svelte-feather-icons/src"
+</script>
+
+<div class="max-w-7xl mx-auto">
+  <a
+    class="flex px-3 py-6 text-sm items-center opacity-60 hover:opacity-100"
+    href="/settings/store"
+  >
+    <ChevronLeftIcon size="24" />
+    <p class="ml-3">Semua Toko / Toko Baru</p>
+  </a>
+</div>
