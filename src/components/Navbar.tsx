6786cf104b69beffe4790ed21d6fc82b953f24e8
Chrisando Pramudhita <chrsep@protonmail.com>
2021-05-21 18:38:08 +0700

    fix link.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -66,19 +66,19 @@ const Navbar: FC<Props> = ({ navigation }) => (
         <Disclosure.Panel className="sm:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1">
             {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className={classNames(
-                  item.current
-                    ? "bg-gray-900 text-white"
-                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                  "block px-3 py-2 rounded-md text-base font-medium"
-                )}
-                aria-current={item.current ? "page" : undefined}
-              >
-                {item.name}
-              </a>
+              <Link key={item.name} href={item.href}>
+                <a
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </a>
+              </Link>
             ))}
           </div>
         </Disclosure.Panel>
