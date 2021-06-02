2c1a9e2cd6a34ddca22b0b69eec031ddc726bd0f
Chrisando Pramudhita <chrsep@protonmail.com>
2021-06-02 17:26:39 +0700

    change text color.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -203,7 +203,7 @@ const UserProfile = () => {
                     onClick={() => signout()}
                     className={classNames(
                       active ? "bg-gray-100" : "",
-                      "block px-4 py-2 text-sm text-gray-700 !text-left w-full"
+                      "block px-4 py-2 text-sm text-red-600 !text-left w-full"
                     )}
                   >
                     Keluar
