bbf13e75524ca594a258a033cb1e19390504d4c5
Chrisando Pramudhita <chrsep@protonmail.com>
2021-06-09 16:13:38 +0700

    tweak style.

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -11,7 +11,7 @@ const ProductList: FC<{ products: Array<Product & { images: ProductImage[]; stor
     {products.map(({ id, images, name, price, store, slug }) => (
       <Link href={`/products/${slug}`}>
         <li key={id} className="relative">
-          <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden border">
+          <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden border bg-white">
             <div className="object-cover pointer-events-none group-hover:opacity-75">
               <Image
                 layout="responsive"
@@ -25,8 +25,8 @@ const ProductList: FC<{ products: Array<Product & { images: ProductImage[]; stor
             <span className="sr-only">Lihat details untuk {name}</span>
           </div>
           <p className="font-ui mt-2 block text-sm text-gray-700 line-clamp-2 mb-2">{name}</p>
-          <p className="font-ui block text-sm text-gray-700">{store.name}</p>
           <p className="block text-sm text-gray-900 font-bold">{toIDR(price)}</p>
+          <p className="font-ui block text-sm text-gray-700">{store.name}</p>
         </li>
       </Link>
     ))}
