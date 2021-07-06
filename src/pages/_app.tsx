9e7a54de75118a4cf0815cf99bdb13fceb3dacd4
Chrisando Pramudhita <chrsep@protonmail.com>
2021-07-06 14:02:27 +0700

    fix cannot find context.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { AppProps } from "next/app"
-import { getSession, Provider } from "next-auth/client"
 import Navbar from "@components/Navbar"
 import { useRouter } from "next/router"
 import "@fontsource/inria-sans/300.css"
@@ -14,21 +13,11 @@ function Grayson({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
   return (
-    <Provider session={pageProps.session}>
-      <div className="bg-gray-100 min-h-screen">
-        {!router.asPath.startsWith("/auth") && <Navbar />}
-        <Component {...pageProps} />
-      </div>
-    </Provider>
+    <div className="bg-gray-100 min-h-screen">
+      {!router.asPath.startsWith("/auth") && <Navbar />}
+      <Component {...pageProps} />
+    </div>
   )
-}
-
-export async function getServerSideProps(ctx) {
-  return {
-    props: {
-      session: await getSession(ctx)
-    }
-  }
 }
 
 export default Grayson
