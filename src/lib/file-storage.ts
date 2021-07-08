740ee23026b0924a6c6dcdc9c1f45061ef709876
Chrisando Pramudhita <chrsep@protonmail.com>
2021-07-08 14:26:57 +0700

    switch from minio to aws-sdk, update deps, removed webpack deps.

--- src/lib/file-storage.ts
+++ src/lib/file-storage.ts
@@ -1,30 +1,25 @@
-import { Client } from "minio"
+import AWS from "aws-sdk"
 
-const { S3_ENDPOINT, S3_PORT, S3_KEY, S3_SECRET, S3_BUCKET } = process.env
+const { S3_ENDPOINT, S3_KEY, S3_SECRET, S3_BUCKET } = process.env
 
 if (!S3_BUCKET) throw new Error("S3 bucket url is not specified")
 if (!S3_SECRET) throw new Error("S3 secret is not specified")
 if (!S3_ENDPOINT) throw new Error("S3 endpoint is not specified")
 if (!S3_KEY) throw new Error("S3 key is not specified")
 
-const minio = new Client({
-  endPoint: S3_ENDPOINT,
-  accessKey: S3_KEY,
-  secretKey: S3_SECRET,
-  port: S3_PORT ? parseInt(S3_PORT, 10) : undefined,
-  useSSL: process.env.NODE_ENV !== "development",
-  region: "sgp1"
+const spacesEndpoint = new AWS.Endpoint(S3_ENDPOINT)
+const s3 = new AWS.S3({
+  endpoint: spacesEndpoint,
+  accessKeyId: process.env.SPACES_KEY,
+  secretAccessKey: process.env.SPACES_SECRET
 })
 
 // eslint-disable-next-line import/prefer-default-export
-export const newSignedUploadUrl = (objectName: string) => {
-  return new Promise((resolve, reject) => {
-    minio.presignedPutObject(S3_BUCKET, objectName, (err, url) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(url)
-      }
-    })
+export const newSignedUploadUrl = (key: string) => {
+  return s3.getSignedUrl("putObject", {
+    Bucket: "grayson-media",
+    ContentType: "text",
+    Expires: 10000,
+    Key: key
   })
 }
