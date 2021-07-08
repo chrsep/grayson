5f60801a7a96954affea110c45249dcac10d3d23
Chrisando Pramudhita <chrsep@protonmail.com>
2021-07-08 17:27:55 +0700

    add cloudflare analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,6 +31,11 @@ class MyDocument extends Document {
           />
           {/* eslint-disable-next-line react/no-danger */}
           <script dangerouslySetInnerHTML={{ __html: sentryInit(process.env.NODE_ENV) }} />
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "2ece74c7bd54443eb949e08561bf65cb"}'
+          />
         </body>
       </Html>
     )
