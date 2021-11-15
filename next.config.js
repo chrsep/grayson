34ba0cb8288ead62c0f5ada5936226714c9b72b3
Chrisando Pramudhita <chrsep@protonmail.com>
2021-11-16 06:58:02 +0700

    remove the rest of the plugins.

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withPlugins = require("next-compose-plugins")
-const nextPWA = require("next-pwa")
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
-
-const withPWA = [
-  nextPWA,
-  {
-    pwa: {
-      dest: "public",
-      disable: process.env.NODE_ENV === "development",
-      // don't precache anything.
-      buildExcludes: [/.*/],
-      publicExcludes: ["!**/*"]
-    }
-  }
-]
-
-const plugins = [withBundleAnalyzer, withPWA]
-
-module.exports = withPlugins(plugins, {
+module.exports = {
   images: {
     formats: ["image/avif", "image/webp"],
     domains: ["localhost", "grayson-media.sgp1.cdn.digitaloceanspaces.com"]
   }
-})
+}
