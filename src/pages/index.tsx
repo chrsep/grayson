a7869382d5d2a6dd48ad7c1e442b7fe0079bbf37
Chrisando Pramudhita <chrsep@protonmail.com>
2021-11-16 06:40:03 +0700

    removes all other pages.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,11 @@ import { findProductsWithPrimaryImagesAndStore } from "@lib/db"
 import { InferGetServerSidePropsType, NextPage } from "next"
 import ProductList from "@components/product-list"
 import React from "react"
-import CategoryNavigation from "@components/category-navigation"
-import SEO from "@components/seo"
 
 const Home: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ products }) => {
   return (
     <div>
-      <SEO
-        title="Temukan UMKM di sekitar-mu"
-        description="Sekitarmu membantu-mu untuk meng-eksplor bisnis UMKM di sekitar-mu"
-      />
-      <CategoryNavigation />
-
-      <main>
-        <ProductList products={products} />
-      </main>
+      <ProductList products={products} />
     </div>
   )
 }
