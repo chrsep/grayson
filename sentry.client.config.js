8b403617722c0066aba347613e94ae405d4e6ca7
Chrisando Pramudhita <chrsep@protonmail.com>
2021-11-16 07:04:37 +0700

    remove more stuffs. load sentry clientside normally.

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,13 +3,14 @@
 // // The config you add here will be used whenever a page is visited.
 // // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 //
-// import * as Sentry from "@sentry/nextjs"
-//
-// const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
-//
-// Sentry.init({
-//   dsn: SENTRY_DSN || "https://b19afc30ece64865ab9f82bf940fa34e@o827330.ingest.sentry.io/5811378"
-//   // Note: if you want to override the automatic release value, do not set a
-//   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-//   // that it will also get attached to your source maps
-// })
+import * as Sentry from "@sentry/nextjs"
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+
+Sentry.init({
+  tracesSampleRate: 0.2,
+  dsn: SENTRY_DSN || "https://b19afc30ece64865ab9f82bf940fa34e@o827330.ingest.sentry.io/5811378"
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+})
