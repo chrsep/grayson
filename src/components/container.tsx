1a4331c0e828cffee4e97f0c452005bed706629d
Chrisando Pramudhita <chrsep@protonmail.com>
2021-11-16 07:02:23 +0700

    remove all other stuffs.

--- src/components/container.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react"
-
-const PageContainer: FC = ({ children }) => {
-  return <div className="sm:px-6 lg:px-8 pt-8 pb-32 mx-auto max-w-7xl">{children}</div>
-}
-
-export default PageContainer
--- src/lib/currency.ts
+++
@@ -1,6 +0,0 @@
-export const toIDR = (price: number) => {
-  return new Intl.NumberFormat("id", {
-    style: "currency",
-    currency: "IDR"
-  }).format(price)
-}
