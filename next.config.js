f9d1d4e29368cf5cc78c7be81dcd2427f27b6d93
Chrisando E. Pramudhita. <chrsep@protonmail.com>
2024-06-26 18:42:10 +0700

    fix deps not installed.

--- next.config.js
+++ next.config.js
@@ -2,25 +2,25 @@
 // const { withSentryConfig } = require("@sentry/nextjs")
 const withPlugins = require("next-compose-plugins")
 const { withPlaiceholder } = require("@plaiceholder/next")
-const nextPWA = require("next-pwa")
+// const nextPWA = require("next-pwa")
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 })
 
-const withPWA = [
-  nextPWA,
-  {
-    pwa: {
-      dest: "public",
-      disable: process.env.NODE_ENV === "development",
-      // don't precache anything.
-      buildExcludes: [/.*/],
-      publicExcludes: ["!**/*"]
-    }
-  }
-]
+// const withPWA = [
+//   nextPWA,
+//   {
+//     pwa: {
+//       dest: "public",
+//       disable: process.env.NODE_ENV === "development",
+//       // don't precache anything.
+//       buildExcludes: [/.*/],
+//       publicExcludes: ["!**/*"]
+//     }
+//   }
+// ]
 
-const plugins = [withBundleAnalyzer, withPlaiceholder, withPWA]
+const plugins = [withBundleAnalyzer, withPlaiceholder]
 // if (process.env.NODE_ENV === "production") plugins.push(withSentryConfig)
 
 module.exports = withPlugins(plugins, {
